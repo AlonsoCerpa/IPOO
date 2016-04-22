f366d17873588400408535735e3153216b4f91d8
Alonso Cerpa <alonso.cerpa@ucsp.edu.pe>
2016-04-22 07:05:20 -0500

    matrices

--- clase_6/Multiplicar_matrices/main.c
+++ clase_6/Multiplicar_matrices/main.c
@@ -1,28 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define VAL 3
 
-void mult_matr(int matr1[][2], int  matr2[][2], int matr_res[][2], int val);
-void print_matr(int matr, int val);
+void mult_matr(int matr1[][VAL], int  matr2[][VAL], int matr_res[][VAL], int val);
+void print_matr(int matr[][VAL], int val);
 
 int main()
 {
-    int val = 2;
-    static int matr1[2][2] = {
-        {4, 2},
-        {3, 8}
+    int val = VAL;
+    static int matr1[VAL][VAL] = {
+        {4, 2, 4},
+        {3, 8, 6},
+        {4, 7, 9}
     };
-    static int matr2[2][2] = {
-        {5, 1},
-        {1, 4}
+    static int matr2[VAL][VAL] = {
+        {5, 1, 6},
+        {1, 4, 3},
+        {4, 6, 7}
     };
-    int matr_res[2][2];
+    int matr_res[VAL][VAL] = {
+        {0, 0, 0},
+        {0, 0, 0},
+        {0, 0, 0}
+    };
 
-    mult_matr(matr1, matr2, matr_res, 2);
+    mult_matr(matr1, matr2, matr_res, 3);
     print_matr(matr_res, val);
     return 0;
 }
 
-void mult_matr(int matr1[][2], int  matr2[][2], int matr_res[][2], int val)
+void mult_matr(int matr1[][VAL], int  matr2[][VAL], int matr_res[][VAL], int val)
 {
     int h, i, j;
 
@@ -35,14 +42,15 @@ void mult_matr(int matr1[][2], int  matr2[][2], int matr_res[][2], int val)
     }
 }
 
-void print_matr(int matr[][2], int val)
+void print_matr(int matr[][VAL], int val)
 {
     int i, j;
 
     for (i = 0; i < val; ++i){
         for (j = 0; j < val; ++j){
-            printf("%d\t", matriz[i][j]);
+            printf("%d ", matr[i][j]);
         }
         printf("\n");
     }
 }
+
