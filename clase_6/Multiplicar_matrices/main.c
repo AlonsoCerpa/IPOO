900c9d3dfface7932736b6c8ec7cb04056546344
AlonsoCerpa <alonso.cerpa@ucsp.edu.pe>
2016-04-22 04:59:12 -0500

    tarea

--- clase_6/Multiplicar_matrices/main.c
+++ clase_6/Multiplicar_matrices/main.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void mult_matr(int matr1[][2], int  matr2[]{2], int matr_res[][2], int row, int col);
-void print_matr(int ma)
+void mult_matr(int matr1[][2], int  matr2[][2], int matr_res[][2], int val);
+void print_matr(int matr, int val);
 
 int main()
 {
+    int val = 2;
     static int matr1[2][2] = {
         {4, 2},
         {3, 8}
@@ -16,22 +17,32 @@ int main()
     };
     int matr_res[2][2];
 
-    mult_matr(matr1, matr2, matr_res, 2, 2);
-    print_matr(matr_res);
+    mult_matr(matr1, matr2, matr_res, 2);
+    print_matr(matr_res, val);
     return 0;
 }
 
-void mult_matr(int matr1[][2], int  matr2[][2], int matr_res[][2], int row, int col)
+void mult_matr(int matr1[][2], int  matr2[][2], int matr_res[][2], int val)
 {
-    int i, j, k;
-    int sum = 0;
-    k = 0;
+    int h, i, j;
 
-    for (j = 0; j < row; j++){
-        for (k = 0; )
-            for (i = 0; i < row; i++){
-                sum += matr1[i][k] * matr2[k][i];
+    for (h = 0; h < val; h++){
+        for (i = 0; i < val; i++){
+            for (j = 0; j < val; j++){
+                 matr_res[h][i]+= matr1[h][j] * matr2[j][i];
             }
-            matr_res[j][k++] = sum;
+        }
+    }
+}
+
+void print_matr(int matr[][2], int val)
+{
+    int i, j;
+
+    for (i = 0; i < val; ++i){
+        for (j = 0; j < val; ++j){
+            printf("%d\t", matriz[i][j]);
+        }
+        printf("\n");
     }
 }
